01bf2e078bbcee6a7c739c6b84eaeb82a8e6166a
Izra Faturrahman <Frizz925@hotmail.com>
2018-01-27 05:03:49 +0700

    Added typings

---
+++ lib/parser.d.ts
@@ -0,0 +1,8 @@
+import { EventEmitter } from 'events';
+
+export default class Parser extends EventEmitter {
+  readonly END: '\r\n';
+  readonly END_LENGTH: 2;
+  private buffer: string;
+  public receive(buffer: Buffer): void;
+}
---
+++ lib/twitter.d.ts
@@ -0,0 +1,58 @@
+import { RequestOptions } from 'http';
+import { RequestResponse } from 'request';
+import { EventEmitter } from 'events';
+
+interface TwitterOptions {
+  consumer_key?: string;
+  consumer_secret?: string;
+  access_token_key?: string;
+  access_token_secret?: string;
+  bearer_token?: string;
+  rest_base?: 'https://api.twitter.com/1.1';
+  stream_base?: 'https://stream.twitter.com/1.1';
+  user_stream_base?: 'https://userstream.twitter.com/1.1';
+  site_stream_base?: 'https://sitestream.twitter.com/1.1';
+  media_base?: 'https://upload.twitter.com/1.1';
+  request_options?: RequestOptions;
+}
+
+type ResponseCallback = (
+  error: any,
+  data: object,
+  response: RequestResponse
+) => void;
+
+type StreamCallback = (stream: EventEmitter) => void;
+
+export default class Twitter {
+  public options: TwitterOptions;
+
+  public constructor(options: TwitterOptions);
+
+  public get(
+    url: string,
+    params?: object | ResponseCallback,
+    callback?: ResponseCallback
+  ): Promise<object> | void;
+
+  public post(
+    url: string,
+    params?: object | ResponseCallback,
+    callback?: ResponseCallback
+  ): Promise<object> | void;
+
+  public stream(
+    method: string,
+    params?: object | StreamCallback,
+    callback?: StreamCallback
+  ): EventEmitter | void;
+
+  private __buildEndpoint(path: string, base: string): string;
+
+  private __request(
+    method: string,
+    path: string,
+    params?: object | ResponseCallback,
+    callback?: ResponseCallback
+  ): Promise<object> | void;
+}
